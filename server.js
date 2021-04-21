916099e70c9c2f4ff74c36c89c797893f4fc1959
Jonas Jason <jonasajason@gmail.com>
2021-04-21 19:35:49 -0400

    find all reservations is close, but not responding to client

--- server.js
+++ server.js
@@ -11,6 +11,46 @@ app.get('/', (req, res) => {
     res.send(`Hello from our GET request!`);
 });
 
+//Find ALL Reservations
+app.get('/getReservations', (req, res) => {
+    console.log("why does this not show up?????");
+        //STEP 1: Read JSON file
+        fs.readFile('reservation.json', (err, data) => {
+            if (err) throw err;
+        
+            let resData = JSON.parse(data);
+        
+            resData.forEach(res => {
+                console.log(res);
+            });
+        
+            resData.sort((res1, res2) => {
+                if (res1.name < res2.name) {
+                    return -1;
+                }
+                if (res2.name < res1.name) {
+                    return 1;
+                }
+                if (res1.num > res2.num) {
+                    return -1;
+                }
+                if (res2.num > res1.num) {
+                    return 1;
+                }
+                return 0;
+            });
+        
+            resData.forEach(res => {
+                console.log(res);
+            });
+        })
+    
+        //Return reservations
+        res.write(`The reservations are: ${reservations}`);
+        console.log("returning reservations");
+        res.sendStatus(200);
+    });
+
 //Add Username
 app.put('/addUser/:userName', (req, res) => {
     const userName = req.params.userName;
@@ -37,7 +77,6 @@ app.put('/addUser/:userName', (req, res) => {
 
 });
 
-<<<<<<< HEAD
 //Find Reservation for Given Username
 app.get('/getReservation/:userName', (req, res) => {
     const userName = req.params.userName;
@@ -48,22 +87,6 @@ app.get('/getReservation/:userName', (req, res) => {
     //match username to reservations
 });
 
-//Find ALL Reservations
-app.get('/getReservations', (req, res) => {
 
-    //STEP 1: Read JSON file
-    const reservations = require("./reservation");
-
-    //Return reservations
-    res.write(`The reservations are: ${reservations}`);
-    console.log("returning reservations");
-=======
-app.get ('/ListRes', function (req, res) {
-    fs.readFile(__dirname + "/" + "reservation.json", 'utf8', function (err, data) {
-        console.log(data);
-        res.end(data);
-    });
->>>>>>> 9ae0c1ce3b1587b287203c05262673859caac16e
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
